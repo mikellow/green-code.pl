bb3c3631fdcc3ee86d961673b9696a30ee304f45
Michal <michal.witczak@gmail.com>
2016-10-08 21:00:20 +0200

    toogle added

--- app/js/askUsForm.js
+++ app/js/askUsForm.js
@@ -2,4 +2,32 @@
 'use strict';
 
     console.log('hello');
+
+    var contactForm = document.getElementById('askUsForm');
+    console.log('contactForm',contactForm);
+
+    contactForm.visible = false;
+    function showContactForm () {
+        contactForm.classList.remove("hidden");
+        contactForm.classList.add("visible");
+        contactForm.visible = true;
+    }
+
+    function hideContactForm () {
+        contactForm.classList.remove("visible");
+        contactForm.classList.add("hidden");
+        contactForm.visible = false;
+    }
+
+    function toggleContactForm () {
+        console.log('toggleFired');
+        if (contactForm.visible) {
+            hideContactForm();
+        } else {
+            showContactForm();
+        }
+    }
+
+    window.toggleContactForm = toggleContactForm;
+
 })();
